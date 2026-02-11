64f0de4245be156f0eeecace91818013da91de05
Yoyoyopo5 <yoyoyopo5@DESKTOP-IT4LOU9>
2026-02-11 06:27:49 -0600

    Add project files.

---
+++ Yoyoyopo5.Helpers.Functional.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Helpers.Functional.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Yoyoyopo5.Helpers.Functional/Combinators/Expand.cs
@@ -0,0 +1,9 @@
+﻿namespace Yoyoyopo5.Helpers.Functional;
+
+public static partial class FunctionalExtensions
+{
+    /// <summary>Converts a <see cref="Step{T}"/> into a <see cref="Step{TIn, TOut}"/> where both type parameters are the same.</summary>
+    /// <param name="step">The same-type step to widen.</param>
+    internal static Step<T, T> Expand<T>(this Step<T> step)
+        => (input, ct) => step(input, ct);
+}
